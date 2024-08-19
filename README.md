f058e8b2b9b568a47b15af0f5c13085dfab3ee41
PitchPost <rodscabral@gmail.com>
2024-08-19 16:40:26 -0300

    ola

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Notas Atletas
+#DEvStart
+
+##Projeto de Cetifiçação 1
+
+Crição uma aplicação capaz de receber o nome e as notas de um atleta, calcular a média e apresentar ao usuário.
+
+Autor
+Rodrigo Cabral @pitchpost (https://github.com/PitchPost)
+Logo
--- nota-atletas.js
+++ nota-atletas.js
@@ -1,21 +1,42 @@
 let atletas = [
- {
-   nome: "Cesar Abascal",
-   notas: [10, 9.34, 8.42, 10, 7.88]
- },
- {
-   nome: "Fernando Puntel",
-   notas:  [8, 10, 10, 7, 9.33]
- },
- {
-   nome: "Daiane Jelinsky",
-   notas: [7, 10, 9.5, 9.5, 8]
- },
- {
-   nome: "Bruno Castro",
-   notas: [10, 10, 10, 9, 9.5]
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas:  [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
+ ];
+ 
+ function calcularMedia(atletas) {
+   atletas.forEach(atleta => {
+     let notaOrdem = atleta.notas.sort((a, b) => a - b);
+     let notasComputa = notaOrdem.slice(1, 4); // Corrected slice syntax
+     let soma = 0;
+     notasComputa.forEach(nota => {
+       soma += nota;
+     });
+     let mediaValida = soma / notasComputa.length;
+     
+     console.log(`Atleta: ${atleta.nome}`);
+     console.log(`Notas Obtidas: ${atleta.notas.join(", ")}`);
+     console.log(`Média Válida: ${mediaValida.toFixed(2)}`); // Rounded to 2 decimal places
+     console.log('');
+   });
  }
-];
-console.log("Bruno Castro")
+ 
+ calcularMedia(atletas);
+ 
+ console.log(calcularMedia) 
 
-            
+ 
+
